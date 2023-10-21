91a1848d0f064934f90f215e5d1f21ded45fcac4
FatimaBadar <fatimabadarq@gmail.com>
2023-10-21 23:28:11 +0500

    basic signup

--- frontend2/pages/_app.js
+++ frontend2/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Header from '@/components/HeaderComponent/Header'
+import Footer from '@/components/FooterComponent/FooterComponent'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header/>
+    <Component {...pageProps} />
+    <Footer/>
+    </>
+    )
 }
---
+++ frontend2/pages/signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignupComponent from '@/components/SignupComponent/SignupComponent'
+
+function signup() {
+  return (
+    <SignupComponent/>
+    )
+}
+
+export default signup
