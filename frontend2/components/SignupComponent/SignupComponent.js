ba1d46f9ed08222d89bfa5dfa645b796f582b5fa
FatimaBadar <fatimabadarq@gmail.com>
2023-10-23 18:58:16 +0500

    .

--- frontend2/components/SignupComponent/SignupComponent.js
+++ frontend2/components/SignupComponent/SignupComponent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './SignupComponent.module.css'
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 function SignupComponent() {
 
@@ -51,15 +52,15 @@ function SignupComponent() {
 
   return (
 
-    <div class="container mx-auto">
-      <div class="flex justify-center px-6 my-12">
-        <div id={styles.glass} class="w-full xl:w-3/4 lg:w-11/12 flex ">
+    <div className="container mx-auto">
+      <div className="flex justify-center px-6 my-12">
+        <div id={styles.glass} className="w-full xl:w-3/4 lg:w-11/12 flex ">
           {/* bg-gray-400 */}
           <div   className="w-full h-auto hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
           >  <img id={styles.signupImage} src="/Images/signupImage.svg" alt="" />
           </div>
 
-          <div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
+          <div className="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
             <div className="px-8 mb-4 text-center">
               <img
                 className="mx-auto h-10 w-auto"
@@ -71,9 +72,9 @@ function SignupComponent() {
               </h2>
             </div>
 
-            {/* class="px-8 pt-6 pb-8 mb-4 bg-white rounded" */}
-            <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={submitSignup} action="#" method="POST">
-            <div class="mb-4">
+            {/* className="px-8 pt-6 pb-8 mb-4 bg-white rounded" */}
+            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={submitSignup} action="#" method="POST">
+            <div className="mb-4">
                 <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
                   First Name
                 </label>
@@ -84,7 +85,7 @@ function SignupComponent() {
                 </div>
               </div>
 
-              <div class="mb-4">
+              <div className="mb-4">
                 <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
                   Last Name
                 </label>
@@ -95,7 +96,7 @@ function SignupComponent() {
                 </div>
               </div>
 
-              <div class="mb-4">
+              <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                   Email address
                 </label>
@@ -106,16 +107,16 @@ function SignupComponent() {
                 </div>
               </div>
 
-              <div class="mb-4">
+              <div className="mb-4">
                 <div className="flex items-center justify-between">
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                     Password
                   </label>
-                  <div id={styles.forget} className="text-sm">
+                  {/* <div id={styles.forget} className="text-sm">
                     <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
                       Forgot password?
                     </a>
-                  </div>
+                  </div> */}
                 </div>
 
                 <div className="mt-2">
@@ -133,21 +134,21 @@ function SignupComponent() {
                   Sign in
                 </button>
                 {/* <button
-									class="w-full px-4 py-2 mt-2 font-bold text-white bg-indigo-600 rounded-full hover:bg-indigo-500 focus:outline-none focus:shadow-outline"
+									className="w-full px-4 py-2 mt-2 font-bold text-white bg-indigo-600 rounded-full hover:bg-indigo-500 focus:outline-none focus:shadow-outline"
 									type="button"
 								>
 								   Sign in
 								</button> */}
               </div>
-              {/* <hr class="mb-6 border-t" /> */}
-              <hr class="my-6 border-t border-gray-300" />
-              <div class="text-center">
-								<a
-									class="inline-block text-sm font-semibold text-indigo-600 hover:text-indigo-500"
+              {/* <hr className="mb-6 border-t" /> */}
+              <hr className="my-6 border-t border-gray-300" />
+              <div className="text-center">
+								<Link
+									className="inline-block text-sm font-semibold text-indigo-600 hover:text-indigo-500"
 									href={"/login"}
 								>
 									Already have an account? Login!
-								</a>
+								</Link>
 							</div>
             </form>
           </div>
