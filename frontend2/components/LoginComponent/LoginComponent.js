08a0242ff5f01d56c936911e55d53019d65c14fa
mohammadBasit29 <k201622@nu.edu.pk>
2023-10-23 06:39:05 -0700

    Login Component Done

--- frontend2/components/LoginComponent/LoginComponent.js
+++ frontend2/components/LoginComponent/LoginComponent.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './LoginComponent.module.css';
 
 function LoginComponent() {
+  const [userData, setUserData] = useState({ email: '', password: '' });
+
+  useEffect(() => {
+    // Add any initialization code if needed
+  }, []);
+
+  const submitLogin = () => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const storedUsers = localStorage.getItem('userData');
+      if (storedUsers) {
+        const users = JSON.parse(storedUsers);
+        const user = users.find((u) => u.email === userData.email);
+        if (user) {
+          if (user.password === userData.password) {
+            alert('Login successful');
+            // You can redirect the user to another page here.
+          } else {
+            alert('Incorrect password');
+          }
+        } else {
+          alert('User not found. Please sign up.');
+        }
+      } else {
+        alert('No users found. Please sign up.');
+      }
+    }
+  };
+
+  const onChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="container mx-auto">
       <div className="flex justify-center px-6 my-12">
-        <div className="w-full xl:w-3/4 lg:w-11/12 flex shadow-md">
-          <div className="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg shadow-md">
-            <img id={styles.signupImage} src="/Images/Professional-People-Silhouette.svg" alt="" />
+        <div id={styles.glass} className="w-full xl:w-3/4 lg:w-11/12 flex">
+          <div className="w-full lg:w-1/2 bg-cover rounded-l-lg">
+            <img id={styles.loginImage} src="/Images/loginImage.svg" alt="" />
           </div>
-          <div className="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none shadow-md">
+
+          <div className="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
             <div className="px-8 mb-4 text-center">
               <img
                 className="mx-auto h-10 w-auto"
@@ -20,13 +56,15 @@ function LoginComponent() {
                 Log in to your account
               </h2>
             </div>
-            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" action="#" method="POST">
+
+            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={submitLogin} action="#" method="POST">
               <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                   Email address
                 </label>
                 <div className="mt-2">
                   <input
+                    onChange={onChange}
                     id="email"
                     name="email"
                     type="email"
@@ -36,19 +74,14 @@ function LoginComponent() {
                   />
                 </div>
               </div>
+
               <div className="mb-4">
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
                 <div className="mt-2">
                   <input
+                    onChange={onChange}
                     id="password"
                     name="password"
                     type="password"
@@ -58,6 +91,7 @@ function LoginComponent() {
                   />
                 </div>
               </div>
+
               <div>
                 <button
                   type="submit"
@@ -66,13 +100,15 @@ function LoginComponent() {
                   Log in
                 </button>
               </div>
+
               <hr className="my-6 border-t border-gray-300" />
+
               <div className="text-center">
                 <a
                   className="inline-block text-sm font-semibold text-indigo-600 hover:text-indigo-500"
                   href="#"
                 >
-                 Don't have Account. Sign Up 
+                  Forgot your password?
                 </a>
               </div>
             </form>
